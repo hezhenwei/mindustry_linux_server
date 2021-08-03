c6764d9e63099bc70bfc4b7226a06d6a48bc37fd
Mike He <3711971@qq.com>
2021-08-03 11:10:23 +0800

    Update run.sh

--- apache_web_server/run.sh
+++ apache_web_server/run.sh
@@ -2,3 +2,7 @@
 
 #sudo docker run -it -p 20080:80 php:7.3.29-apache /bin/bash
 sudo docker run -d -p 20080:80 --name webserver -v /home/steamuser/docker/apache/html:/var/www/html -v /home/steamuser/mindustry_maps_share:/var/www/maps php:7.3.29-apache
+
+sudo docker exec webserver chmod 777 /var/www/maps
+
+
