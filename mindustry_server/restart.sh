da996534413c2cee2b2ee90ccca3ccf4d2a2a7db
Mike He <3711971@qq.com>
2021-08-03 10:58:53 +0800

    Update restart.sh

--- mindustry_server/restart.sh
+++ mindustry_server/restart.sh
@@ -17,3 +17,11 @@ sleep 10
 pkill -9 screen
 sleep 2
 cd /home/steamuser/mindustry_server129/ && screen -d -m -S "mindustry_server" -L ./run_server.sh
+
+
+
+# Because the steam client seems drop occasionally,
+# try reboot every morning.
+# add following lines with "crontab -e"
+# to restart it at 04:00 everyday 
+# 0 4 * * * /home/steamuser/mindustry_server129/restart.sh
