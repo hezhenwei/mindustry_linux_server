ea8e473b796df892ea3c77197a966a3791afdc9f
Mike He <3711971@qq.com>
2021-08-03 10:57:01 +0800

    Create restart.sh

---
+++ mindustry_server/restart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+screen_name=$"mindustry_server"
+cmd1=$"stop";
+cmd2=$"exit";
+screen -x -S $screen_name -p 0 -X stuff $'\n'
+screen -x -S $screen_name -p 0 -X stuff "$cmd1"
+screen -x -S $screen_name -p 0 -X stuff $'\n'
+screen -x -S $screen_name -p 0 -X stuff "$cmd2"
+screen -x -S $screen_name -p 0 -X stuff $'\n'
+
+echo "$cmd1" is called.
+echo "$cmd2" is called.
+echo `date` "stopping" > /tmp/min.txt
+
+sleep 10
+pkill -9 screen
+sleep 2
+cd /home/steamuser/mindustry_server129/ && screen -d -m -S "mindustry_server" -L ./run_server.sh
