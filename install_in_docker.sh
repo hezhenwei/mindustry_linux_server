6c31c0652fb44bf1f1201456ab51ce6bafa992ad
Mike He <3711971@qq.com>
2021-08-10 23:52:09 +0800

    Update install_in_docker.sh

--- install_in_docker.sh
+++ install_in_docker.sh
@@ -4,7 +4,7 @@ sudo apt install docker -y
 
 sudo docker pull adoptopenjdk/openjdk11:jre
 
-sudo docker create  --name mindustry_server  adoptopenjdk/openjdk11:jre
+sudo docker create  --name mindustry_server  adoptopenjdk/openjdk11:jre mtdserver1292
 
-sudo docker run 
+sudo docker start
 
