57630ba3227843222d60e217c19906446622d153
Mike He <3711971@qq.com>
2021-08-11 15:14:30 +0800

    Update install_in_docker.sh

--- install_in_docker.sh
+++ install_in_docker.sh
@@ -5,17 +5,16 @@ sudo apt install docker -y
 sudo docker pull adoptopenjdk/openjdk11:jre
 
 # download the server tar file and unzip first then
-mkdir ./mindustry_server/config
-mkdir ./mindustry_server/config/mods
+mkdir ./mindustry_server_latest/config
+mkdir ./mindustry_server_latest/config/mods
 wget https://github.com/way-zer/ScriptAgent4MindustryExt/releases/download/v2.9.0/ScriptAgent4Mindustry-2.9.jar
-mv ./ScriptAgent4Mindustry-2.9.jar ./mindustry_server/config/mods
+mv ./ScriptAgent4Mindustry-2.9.jar ./mindustry_server_latest/config/mods
 wget https://github.com/way-zer/ScriptAgent4MindustryExt/releases/download/v2.9.0/ScriptAgent4Mindustry-2.9.0-scripts.zip
-mv ./ScriptAgent4Mindustry-2.9.0-scripts ./mindustry_server/config/scripts
+unzip ./ScriptAgent4Mindustry-2.9.0-scripts.zip -d ScriptAgent4Mindustry-2.9.0-scripts
+mv ./ScriptAgent4Mindustry-2.9.0-scripts ./mindustry_server_latest/config/scripts
 wget https://github.com/shugen002/MDTSteamLobbyPlugin/releases/download/1.2.0/MDTSteamLobbyPlugin.jar
-mv ./MDTSteamLobbyPlugin.jar ./mindustry_server/config/mods
+mv ./MDTSteamLobbyPlugin.jar ./mindustry_server_latest/config/mods
 
-wget https://raw.githubusercontent.com/hezhenwei/mindustry_linux_server/main/Dockerfile
-sudo docker build ./ -t -t mtdserver:129.2-plugin-lobby-wayzer
+sudo docker build ./ -t mtdserver:129.2-plugin-lobby-wayzer
 
-sudo docker start
-
+sudo docker run --name mymtd mtdserver:129.2-plugin-lobby-wayzer /bin/bash
