b932ead847261af2fd62ce649304f89ba2e8db03
Mike He <3711971@qq.com>
2021-08-12 12:14:15 +0800

    Update install_in_docker.sh

--- install_in_docker.sh
+++ install_in_docker.sh
@@ -12,9 +12,14 @@ mv ./ScriptAgent4Mindustry-2.9.jar ./mindustry_server_latest/config/mods
 wget https://github.com/way-zer/ScriptAgent4MindustryExt/releases/download/v2.9.0/ScriptAgent4Mindustry-2.9.0-scripts.zip
 unzip ./ScriptAgent4Mindustry-2.9.0-scripts.zip -d ScriptAgent4Mindustry-2.9.0-scripts
 mv ./ScriptAgent4Mindustry-2.9.0-scripts ./mindustry_server_latest/config/scripts
-wget https://github.com/shugen002/MDTSteamLobbyPlugin/releases/download/1.2.0/MDTSteamLobbyPlugin.jar
-mv ./MDTSteamLobbyPlugin.jar ./mindustry_server_latest/config/mods
 
-sudo docker build ./ -t mtdserver:129.2-plugin-lobby-wayzer
+#echo getting steam lobby plugin
+#wget https://github.com/shugen002/MDTSteamLobbyPlugin/releases/download/1.2.0/MDTSteamLobbyPlugin.jar
+#mv ./MDTSteamLobbyPlugin.jar ./mindustry_server_latest/config/mods
 
-sudo docker run --name mymtd mtdserver:129.2-plugin-lobby-wayzer /bin/bash
+sudo docker build ./ -t hezhenwei/mtdserver:129.2-plugin-lobby-wayzer
+
+sudo docker create -it -p 6568:6567 --name mymtd hezhenwei/mtdserver:129.2-plugin-lobby-wayzer
+
+sudo docker start mymtd
+~
